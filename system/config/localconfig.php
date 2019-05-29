dd0874781108726ec9b48ea869b35663453eebb0
Nermin Elkasovic <nermin.elkas@gmail.com>
2019-05-29 16:28:53 +0200

    Second commit

--- system/config/localconfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-### INSTALL SCRIPT START ###
-$GLOBALS['TL_CONFIG']['licenseAccepted'] = true;
-$GLOBALS['TL_CONFIG']['installPassword'] = '$2y$10$Warkl6qRTu1WjucuiTxnx..HqawrmtBDYtfn7w5oZM/9jtgNMcjIW';
-$GLOBALS['TL_CONFIG']['adminEmail'] = 'info@qlick.ch';
-### INSTALL SCRIPT STOP ###
